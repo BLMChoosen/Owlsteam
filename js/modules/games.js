d4d4f5bbeaa24622b01d199ac4fbfb66a0c4d940
Choosen <89951899+BLMChoosen@users.noreply.github.com>
2025-05-13 14:02:28 -0300

    tabela v2

---
+++ js/modules/games.js
@@ -0,0 +1,51 @@
+window.siteGames = [
+    {
+        title: "Cyber Ninja",
+        status: "Pré-venda",
+        price: 59.99,
+        requirements: "Windows 10, 8GB RAM, GTX 1050",
+        icon: "bxs-rocket",
+        reqIcon: "bxs-cog"
+    },
+    {
+        title: "Hacker Quest",
+        status: "Lançamento",
+        price: 49.99,
+        requirements: "Windows 10, 4GB RAM, Intel HD",
+        icon: "bxs-bolt",
+        reqIcon: "bxs-chip"
+    },
+    {
+        title: "Future Race",
+        status: "Disponível",
+        price: 39.99,
+        requirements: "Windows 8, 6GB RAM, GTX 750",
+        icon: "bxs-car",
+        reqIcon: "bxs-tachometer"
+    },
+    {
+        title: "Stealth Agent",
+        status: "Disponível",
+        price: 44.99,
+        requirements: "Windows 10, 8GB RAM, GTX 960",
+        icon: "bxs-user-voice",
+        reqIcon: "bxs-shield"
+    },
+    {
+        title: "Space Explorer",
+        status: "Pré-venda",
+        price: 69.99,
+        requirements: "Windows 10, 16GB RAM, RTX 2060",
+        icon: "bxs-space-bar",
+        reqIcon: "bxs-planet"
+    },
+    {
+        title: "Zombie Apocalypse",
+        status: "Lançamento",
+        price: 54.99,
+        requirements: "Windows 10, 8GB RAM, GTX 1050 Ti",
+        icon: "bxs-zombie",
+        reqIcon: "bxs-skull"
+    }
+    
+];
